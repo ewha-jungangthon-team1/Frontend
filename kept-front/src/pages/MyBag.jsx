314589df03d2661b28d53941455dbefc84a876cb
HaHuiseo <huiseoha425@ewha.ac.kr>
2026-08-15 20:11:07 +0900

    feat(navigation): GNB 배경 수정 및 My Bag 라우팅 추가

---
+++ kept-front/src/pages/MyBag.jsx
@@ -0,0 +1,15 @@
+function MyBag({ onOpenMenu }) {
+  return (
+    <main>
+      <header>
+        <h1>My Bag (GUI 완료 후 작업 예정)</h1>
+
+        <button type="button" onClick={onOpenMenu}>
+          메뉴
+        </button>
+      </header>
+    </main>
+  );
+}
+
+export default MyBag;
